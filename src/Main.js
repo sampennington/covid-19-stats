be055be8ca140d7b36ed605eeb8ada8afc19ae5a
Manuel Alonge <manuel.alonge@gmail.com>
2020-05-25 10:44:00 +0200

    styles added

--- src/Main.js
+++ src/Main.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import useStats from './useStats';
-import './index.css';
 
 // useState --> https://it.reactjs.org/docs/hooks-state.html
 // useEffect --> https://it.reactjs.org/docs/hooks-effect.html
 // Async / Await --> https://javascript.info/async-await
 
 function Stats() {
+    
+    const statGrid = {
+        marginTop: '170px',
+        display: 'grid',
+        gridTemplateColumns: 'repeat(3, 1fr)',
+        gridGap: '1rem'
+    };
+
+    const statBlock = {
+        backgroundColor: 'azure',
+        fontSize: '2rem'
+    };
+
     const url = 'https://covid19.mathdro.id/api/countries/italy';
+    
     const stats = useStats(url);
+    
     if (!stats) return <p>Caricamento...</p>;
     return (
-        <div>
-            <div className="statBlock">
-                <h3>Totale positivi:</h3>
+        <div style={statGrid}>
+            <div style={statBlock}>
+                <h3>Casi totali:</h3>
                 <span>{stats.confirmed.value}</span>
             </div>
-            <div className="statBlock">
+            <div style={statBlock}>
                 <h3>Decessi:</h3>
                 <span>{stats.deaths.value}</span>
             </div>
-            <div className="statBlock">
+            <div style={statBlock}>
                 <h3>Guariti:</h3>
                 <span>{stats.recovered.value}</span>
             </div>
@@ -31,7 +45,7 @@ function Stats() {
 function IndexPage() {
     return (
         <div>
-            <Stats url = 'https://covid19.mathdro.id/api/countries/italy'>
+            <Stats url='https://covid19.mathdro.id/api/countries/italy'>
 
             </Stats>
         </div>
--- src/useStats.js
+++ src/useStats.js
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react';
 
-function useStats(url) {
+function useStats() {
     const [stats, setStats] = useState();
 
     useEffect(() => {
         async function fetchData() {            
-            const data = await fetch(url)
+            const data = await fetch('https://covid19.mathdro.id/api/countries/italy')
                 .then(res => res.json());
              setStats(data);
              // console.log(data);
