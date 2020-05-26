2b9610f60a76bd8c237f7a84bd1b675b349bf54c
Manuel Alonge <manuel.alonge@gmail.com>
2020-05-26 16:36:03 +0200

    graph issue

--- src/Graph.js
+++ src/Graph.js
@@ -2,71 +2,67 @@ import React, { useState, useEffect } from 'react';
 import {
     LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
+import useStats from './useStats';
 
 function Graph() {
-    const [data, setData] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
+    const url = `https://covid19.mathdro.id/api/countries/italy`;
+    const stats = useStats(url);
+    if (!stats) return <p>Loading...</p>;
 
-        const runCall = async () => {
-            let apiValue = await fetchData();
-            const header = apiValue.split('\n').slice(0).map(line => (line.split(',')))[0].slice(4);
-            console.log(header);
+    /*let apiValue = await fetch(`https://covid19.mathdro.id/api/countries/italy`);
+    const header = apiValue;
+    console.log(header);
 
-            const chartData = [];
+    const chartData = [];
 
-            for (let i = 0; i < header.length; i++) {
-                const newRow = [];
-                newRow.name = [header[i]];
-                newRow.data = parseInt(header[i]);
-                chartData.push(newRow);
-            }
-            console.log(chartData);
-            setData(chartData);
-            setIsLoading(false);
-            return null;
-        }
+    for (let i = 0; i < header.length; i++) {
+        const newRow = [];
+        newRow.name = [header[i]];
+        newRow.data = parseInt(header[i]);
+        chartData.push(newRow);
+    }
+    console.log(chartData);
+    setData(chartData);
+    setIsLoading(false);
+    return null;
 
-        const fetchData = async () => {
-            const requestOption = {
-                method: "GET",
-                redirect: "follow"
-            };
 
-            const apiUrl = `https://covid19.mathdro.id/api/countries/italy`;
-
-            try {
-                const response = await fetch(apiUrl, requestOption);
-                return response.ok ? response.text() : null;
-            } catch (err) {
-                console.log(err);
-                return null;
-            };
+    const fetch = async () => {
+        const requestOption = {
+            method: "GET",
+            redirect: "follow"
         };
-        runCall();
-    }, []);
+
+        const apiUrl = `https://covid19.mathdro.id/api/countries/italy`;
+
+        try {
+            const response = await fetch(apiUrl, requestOption);
+            return response.ok ? response.text() : null;
+        } catch (err) {
+            console.log(err);
+            return null;
+        };
+    };
+    runCall();*/
 
     return (
         <div className="chart-wrapper">
-            {isLoading ?
-                <div>Loading...</div> :
-                <LineChart
-                    width={800}
-                    height={500}
-                    data={data}
-                    margin={{
-                        top: 5, right: 30, left: 20, bottom: 5,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Line type="monotone" dataKey="data" stroke="blue" activeDot={{ r: 8 }} />
-                </LineChart>
-            };
+            <LineChart
+                width={800}
+                height={500}
+                data={stats}
+                margin={{
+                    top: 5, right: 30, left: 20, bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="stats" stroke="blue" activeDot={{ r: 8 }} />
+            </LineChart>            
         </div >
     );
 }
